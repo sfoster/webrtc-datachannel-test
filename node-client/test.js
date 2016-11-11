753e541278110f89dbc39b9b9bced54f2e6cbe10
Sam Foster <sfoster@mozilla.com>
2016-11-10 16:01:12 -0800

    Playing with mapping emoticons to unicode emoji characters

--- node-client/test.js
+++ node-client/test.js
@@ -1,32 +1,10 @@
-var Sound = require('node-aplay');
-var tmp = require('tmp');
-var fs = require('fs');
+var emojiLookup = require('./emoticon2emoji.js');
+console.log(' :)', emojiLookup.get(':)'));
+console.log(' :(', String.fromCodePoint( emojiLookup.getCodePoint(':(') ));
+console.log('(thumbsup)', emojiLookup.get('(thumbsup)'));
+console.log('(clap)', emojiLookup.get('(clap)'));
 
-function playBufferAsAudio(buf, contentType) {
-  // create tmp file
-  // write the buffer to it
-  // pass that tmp file to an external player
-
-  tmp.file(function (err, path, fd, cleanup) {
-    if (err) throw err;
-
-    console.log("File: ", path);
-    fs.writeFile(path, buf, function(err) {
-      if (err) throw err;
-      console.log('wrote file, now play it');
-      var audio = new Sound(path);
-      audio.on('complete', function() {
-        console.log('Done with playback');
-        // if we don't need the file anymore we could manually call the cleanupCallback
-        // cleanup();
-      })
-      audio.play();
-    });
-  });
-}
-
-
-fs.readFile('./bird.wav', function(err, buf) {
-  if (err) throw err;
-  playBufferAsAudio(buf, '');
-});
+// commands (which eventually map to hardware actions):
+// emoji selector: just accept e.g. emoji:(thumbsup)
+// record audio: record:[ENTER]
+// [ENTER] to end recording
